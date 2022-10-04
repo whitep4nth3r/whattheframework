07dd6c9761896941b08050ea7da999c9ef6c1cb0
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-10-04 10:18:28 +0100

    Small update to framework data

--- src/data/frameworks/gatsby.ts
+++ src/data/frameworks/gatsby.ts
@@ -1,11 +1,9 @@
 import { React } from "./react";
 import { Framework, WebsiteType, AppType, FrameworkFeatures } from "../types";
 
-//subframework — React
-
 export const Gatsby: Framework = {
   websiteTypesInOrderOfPriority: [WebsiteType.Hybrid, WebsiteType.Static],
-  appTypesInOrderOfPriority: [AppType.Spa],
+  appTypesInOrderOfPriority: [AppType.Spa, AppType.Mpa],
   features: [FrameworkFeatures.TypeScript, FrameworkFeatures.Cli],
   usesFrameworks: [React],
   templatingEngines: [],

51038fcd8809064ff767ddb16ce5abc54efe9333
Sai Madhan <saimadhan@outlook.in>
2022-10-20 07:11:45 +0000

    style: Home page design

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: "Silkscreen",
+        sans: "Roboto",
+      },
+    },
+  },
+  plugins: [],
+};
